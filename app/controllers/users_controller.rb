7f3cc50fdbfd7e73a743ef2a3a87570f60c2c467
Adam C. Baker <adam.durandal@gmail.com>
2010-12-28 00:33:46 -0600

    Added flash handling.
    I added a flash handler to the app layout template and the css
    to style it to scaffold.css. I also removed unnecessary view specs.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,14 @@ class UsersController < ApplicationController
 
   # GET /users/1
   # GET /users/1.xml
+  # GET /users/1.json
   def show
     @user = User.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @user }
+      format.json { render :json => @user }
     end
   end
 
