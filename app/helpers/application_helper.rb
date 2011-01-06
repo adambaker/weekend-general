e30cfe11bbd07eee6439d7ec070baae0604a9dce
Adam C. Baker <adam.durandal@gmail.com>
2011-01-06 15:08:52 -0600

    Removed helper methods that duplicated Rails built-in helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,27 +7,9 @@ module ApplicationHelper
     end
   end
   
-  def truncate(message, num_chars = 100)
-    if message.size <= num_chars
-      message
-    else
-      message[0...num_chars] + '...'
-    end
-  end
-  
-  def strip_tags(text)
-    text.gsub tag_regex, ''
-  end
-  
-  def restore_tags(text)
-    text.gsub tag_regex, '<\1>'
-  end
-  
-  private
-    def tag_regex
-      allowed_tags = %w[em i strong b code samp tt s strike sup sub a img
+  def allowed_tags
+    %w[em i strong b code samp tt s strike sup sub a img
         blockquote pre ul ol li table tr td p]
-      tag = "(#{allowed_tags.join('|')})"
-      /&lt;(\/?#{tag}( .*?)?)&gt;/o
-    end
+  end
+  
 end
