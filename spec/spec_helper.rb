bd439f399e36d205f7bd2dd2eb1586b608609a9b
Adam C. Baker <adam.durandal@gmail.com>
2011-01-22 23:48:07 -0600

    Fixed a link validation and some display bugs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -65,7 +65,7 @@ Cover for events ranges from free to about $1200, so be prepared. Typically host
   end
   
   def valid_urls
-    [ 'http://foo.bar.com', 'www.go-here-now.il', 'find.us',
+    [ 'http://foo.bar.com', 'www.go-here-now.il', 'find.us/',
       'punch.com/users/1/get?foo=1&bar=2', 'http://go.co?goo=foo',
       'am.de/pan_da+all/man?squill=seminal', 'foo.co?e=foo%40goo.co', '' ]
   end
