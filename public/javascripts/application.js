a24e7076009e28cf16ed222a04c6b1755c2275c1
Adam C. Baker <adam.durandal@gmail.com>
2011-02-06 19:10:02 -0600

    Made search bar clear on focus.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready( function()
+  {
+    var $search_bar = $("#small_search_text");
+    $search_bar.focus(function(){ $search_bar.attr("value", "");})
+    $search_bar.blur( function()
+      {
+        if($search_bar.attr('value') == "") 
+          $search_bar.attr("value","search");
+      }
+    );
+  }
+);
